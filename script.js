1983103bd1ec71660573b28020cab114d6a8e399
Copperage <ethanhud6@gmail.com>
2023-04-05 08:10:05 +0100

    changed the displaylibrary this is so whenever you make a new book it doesnt reload the whole library

--- script.js
+++ script.js
@@ -19,47 +19,43 @@ function displayLibrary() {
 	const libraryList = document.querySelector('.book-list-container');
 
 	libraryList.innerHTML = '';
-
-	bookLibrary.forEach((book) => {
-		let bookList = document.createElement('ul');
-		bookList.classList.add('card');
-
-		const removeButtonContainer = document.createElement('div');
-		removeButtonContainer.classList.add('remove-button-container');
-
-		let removeButton = document.createElement('button');
-		removeButton.textContent = 'X';
-		removeButton.addEventListener('click', () => {
-			removeBookFromLibrary(book);
-			bookList.remove();
-		});
-
-		const readCheckbox = document.createElement('input');
-		readCheckbox.type = 'checkbox';
-		readCheckbox.checked = book.read;
-		readCheckbox.addEventListener('change', () => {
-			book.toggleReadStatus();
-			displayLibrary();
-		});
-
-		bookList.innerHTML = `
-		<li>${book.title}</li>
-		<li>${book.author}</li>
-		<li>${book.pages}</li>
-		<li>${book.read ? 'Read' : 'Not read'}</li>
-	  `;
-
-		bookList.children[3].appendChild(readCheckbox);
-		removeButtonContainer.appendChild(removeButton);
-		bookList.appendChild(removeButtonContainer);
-		libraryList.appendChild(bookList);
-	});
 }
 
 function addBookToLibrary(title, author, pages, read) {
 	const newBook = new Book(title, author, pages, read);
 	bookLibrary.push(newBook);
-	displayLibrary();
+
+	let bookList = document.createElement('ul');
+	bookList.classList.add('card');
+
+	const removeButtonContainer = document.createElement('div');
+	removeButtonContainer.classList.add('remove-button-container');
+
+	let removeButton = document.createElement('button');
+	removeButton.textContent = 'X';
+	removeButton.addEventListener('click', () => {
+		removeBookFromLibrary(newBook);
+		bookList.remove();
+	});
+
+	const readCheckbox = document.createElement('input');
+	readCheckbox.type = 'checkbox';
+	readCheckbox.checked = newBook.read;
+	readCheckbox.addEventListener('change', () => {
+		newBook.toggleReadStatus();
+	});
+
+	bookList.innerHTML = `
+	  <li>${newBook.title}</li>
+	  <li>${newBook.author}</li>
+	  <li>${newBook.pages}</li>
+	  <li>${newBook.read ? 'Read' : 'Not read'}</li>
+	`;
+
+	bookList.children[3].insertAdjacentElement('afterbegin', readCheckbox);
+	removeButtonContainer.appendChild(removeButton);
+	bookList.appendChild(removeButtonContainer);
+	document.querySelector('.book-list-container').appendChild(bookList);
 }
 
 function removeBookFromLibrary(book) {
